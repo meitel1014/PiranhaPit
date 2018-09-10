5702757ea073835c3ebdabac2907955281d9749a
MEITEL <meitel1014@gmail.com>
2018-09-10 23:52:48 +0900

    added files

--- script.py
+++ script.py
@@ -28,37 +28,42 @@ def overwrite(file, timer):
     file.truncate()
 
 
-tweet = ""
-with open('timer.txt', 'r+') as file:
-    timer = int(file.read())
-    if timer == -1:
-        next = getSchedule()
-        if next == -1:
-            tweet = "おっ、ナワバリショッツルないやんけ。"
+def tweet():
+    tweet = ""
+    with open('timer.txt', 'r+') as file:
+        timer = int(file.read())
+        if timer == -1:
+            next = getSchedule()
+            if next == -1:
+                tweet = "おっ、ナワバリショッツルないやんけ。"
+            else:
+                tweet = "おっ、" + str(next - 2) + "時間後ナワバリショッツルやんけ。"
+                overwrite(file, next - 1)
+        elif timer == 0:
+            tweet = "おっ、ナワバリショッツル終わったやんけ。"
+            overwrite(file, getSchedule() - 1)
+        elif timer == 1 or timer == 2:
+            tweet = "おっ、ナワバリショッツルやんけ。"
+            overwrite(file, timer - 1)
         else:
-            tweet = "おっ、" + str(next) + "時間後ナワバリショッツルやんけ。"
-            overwrite(file, next - 1)
-    elif timer == 0:
-        tweet = "おっ、ナワバリショッツル終わったやんけ。"
-        overwrite(file, getSchedule() - 1)
-    elif timer == 1 or timer == 2:
-        tweet = "おっ、ナワバリショッツルやんけ。"
-        overwrite(file, timer - 1)
-    else:
-        tweet = "おっ、" + str(timer) + "時間後ナワバリショッツルやんけ。"
-        overwrite(file, timer - 1)
+            tweet = "おっ、" + str(timer - 2) + "時間後ナワバリショッツルやんけ。"
+            overwrite(file, timer - 1)
 
-print(tweet)
+    print(tweet)
 
-twitter = OAuth1Session(
-    os.environ["CONSUMER_KEY"], os.environ["CONSUMER_SECRET"],
-    os.environ["ACCESS_TOKEN_KEY"], os.environ["ACCESS_TOKEN_SECRET"])
+    twitter = OAuth1Session(
+        os.environ["CONSUMER_KEY"], os.environ["CONSUMER_SECRET"],
+        os.environ["ACCESS_TOKEN_KEY"], os.environ["ACCESS_TOKEN_SECRET"])
 
-twparams = {"status": tweet}
-req = twitter.post(
-    "https://api.twitter.com/1.1/statuses/update.json", params=twparams)
+    twparams = {"status": tweet}
+    req = twitter.post(
+        "https://api.twitter.com/1.1/statuses/update.json", params=twparams)
 
-if req.status_code == 200:  #正常投稿出来た場合
-    print("Success.")
-else:  #正常投稿出来なかった場合
-    print("Failed. : %d" % req.status_code)
+    if req.status_code == 200:  #正常投稿出来た場合
+        print("Success.")
+    else:  #正常投稿出来なかった場合
+        print("Failed. : %d" % req.status_code)
+
+
+if __name__ == "__main__":
+    tweet()
